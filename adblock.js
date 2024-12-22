96117efb2c851c0100e63db9862f08b58585261d
Shasin Regmi <shasinregmi@gmail.com>
2024-12-22 20:18:38 +0545

    after a week

--- adblock.js
+++ adblock.js
@@ -1,1 +1,14 @@
-console.log('adblock.js loaded');
+const hideAds = () => {
+const ads = document.querySelectorAll("shreddit-ad-post");
+
+ads.forEach((ad) => {
+    ad.style.display = "none";
+    ad.style.visibility = "hidden";
+});
+};
+
+const resizeObserver = new ResizeObserver(() => {
+hideAds();
+});
+
+resizeObserver.observe(document.body);
